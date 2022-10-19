fc2cf90675de8ca5126c7240213a5d3b36bdc88e
Bas Maw <sebastien.maw@madetech.com>
2022-10-19 17:13:13 +0100

    more refactoring spec after 3rd pass

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,19 +1,22 @@
 require './lib/tennis'
 
+def expect_score_to_eq(expected_score)
+  expect(tennis_scorer.score).to eq(expected_score)
+end
+
 describe 'Tennis Scorer' do
   let(:tennis_scorer) { TennisScorer.new}
   it 'can score a new game where no player has score yet' do
-    expect(tennis_scorer.score).to eq('Love-Love')
+    expect_score_to_eq('Love-Love')
   end
 
   it 'can score a game where player one has scored once' do
     tennis_scorer.won_point
-    expect(tennis_scorer.score).to eq('Fifteen-Love')
+    expect_score_to_eq('Fifteen-Love')
   end
   
   it 'can score a game where player one has scored twice' do
-    tennis_scorer.won_point
-    tennis_scorer.won_point
-    expect(tennis_scorer.score).to eq('Thirty-Love')
+    2.times { tennis_scorer.won_point }
+    expect_score_to_eq('Thirty-Love')
   end
 end
